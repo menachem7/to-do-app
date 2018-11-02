1f496e366d3e88989a4d4cdcd93d9300c905ebc7
Menachem Polonsky <m@mbenleonardo.com>
2018-11-02 14:55:32 -0400

    Delete button created but not functional

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 window.onload = function () {
   onReady();
+
+  var deleteLi = document.getElementById('li')
+  if (deleteButton) {
+    deleteLi.remove();
+  }
 };
 
-function onReady () {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+const addToDoForm = document.getElementById('addToDoForm');
+const newToDoText = document.getElementById('newToDoText');
+const toDoList = document.getElementById('toDoList');
 
+// create a delete button for the li
+let deleteButton = document.createElement ('button');
+
+// text for the delete button
+let deleteContent = document.createTextNode ('Delete');
+
+// assign text inside delete button
+deleteButton.appendChild(deleteContent);
+
+function onReady () {
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -25,13 +39,16 @@ function onReady () {
     // set the title
     newLi.textContent = title;
 
-    // attach the checkbox to the Li
+    // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    //attach delete button to the li
+    newLi.appendChild(deleteButton);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
-    // empty the imput
+    // empty the input
     newToDoText.value = '';
   });
 };
